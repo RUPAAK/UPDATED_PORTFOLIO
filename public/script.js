d85d015af665764fbdae2bd1b3f9baad272bbbf9
Rupak <rupakt525@gmail.com>
2021-06-13 23:15:47 +0545

    fixes

--- public/script.js
+++ public/script.js
@@ -3,6 +3,7 @@ const night= document.getElementById("night");
 const seemore= document.querySelector('.seemore')
 const hiddenProjects= document.querySelector('.hidden_projects')
 const form= document.querySelector('form')
+const input = document.querySelectorAll('input')
 
 hiddenProjects.style.height= '0px'
 
@@ -16,12 +17,15 @@ if ( window.history.replaceState ) {
 night.addEventListener('click', ()=>{   
     document.body.style.background= "#252525";
     document.body.style.color= "white";
+    input.color= "black"
+    form.style.background= "rgb(99, 97,  97)"
     night.style.display= "none";
     day.style.display= "block";
 })
 day.addEventListener('click', ()=>{   
     document.body.style.background= "white";
     document.body.style.color= "#252525";
+    form.style.background= "rgb(0, 0, 0))"
     night.style.display= "block";
     day.style.display= "none";
 })
